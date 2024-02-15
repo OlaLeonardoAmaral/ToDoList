2e92e9c78a737b0868daef86eb55aeb43e1e7964
Leonardo <leonardo3442@hotmail.com>
2024-02-15 17:50:32 -0300

    commit inicial com os controllers repositories models e autenticacao

---
+++ src/main/java/br/com/leonardoamaral/todolist/task/TaskController.java
@@ -0,0 +1,50 @@
+package br.com.leonardoamaral.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private ITaskRepository taskRepository;
+
+    @PostMapping("/")
+    public ResponseEntity createTask(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID) idUser);
+
+        var currentDate = LocalDateTime.now();
+        if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.badRequest()
+                    .body("A data de início deve ser maior do que a data atual");
+        }
+
+        if(taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.badRequest()
+                    .body("A data de início deve ser menor do que a data de término"); 
+        }
+
+        var task = taskRepository.save(taskModel);
+        return ResponseEntity.ok().body(task);
+    }
+
+    @GetMapping("/")
+    public List<TaskModel> getAllTasks(HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        var tasks = this.taskRepository.findTaskByIdUser((UUID)idUser);
+        return tasks;
+    }
+}
